2a43e0da28ce2c86a866e4aae04a05c7acbbb360
heathkev <kevintuazon45@gmail.com>
2020-11-17 22:15:58 +0800

    added mongo db connection used nodemon on scripts install mongo db dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:1T0eKCLUUAh3ykD2@cluster0.ksw13.mongodb.net/sample_mflix?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', err => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
