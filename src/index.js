8d192e5bc06c778c1d98f2938200d2eead6d85da
heathkev <kevintuazon45@gmail.com>
2020-11-19 00:03:58 +0800

    creates a new model track implemented a track routes imports track

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:1T0eKCLUUAh3ykD2@cluster0.ksw13.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
