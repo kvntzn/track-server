d550a514b4bbb04df9ab8d442aabf38a83e10864
heathkev <kevintuazon45@gmail.com>
2020-11-18 23:47:15 +0800

    added function for comparing password User model Added sign in functionality

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,10 +12,31 @@ router.post('/signup', async (req, res) => {
         const user = new User({ email, password });
         await user.save();
 
-        const token = jwt.sign({ userId: user._id}, 'MY_SECRET_KEY');
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
         res.send({ token });
     } catch (error) {
         res.status(422).send(error.message);
+    }
+});
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(422).send({ error: 'Invalid password or email' });
+    }
+
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+        res.send({ token });
+    } catch (error) {
+        return res.status(422).send({ error: 'Invalid password or email' });
     }
 });
 
